fd02da9569b4e960cdba54f08fe98ef708059a2e
Hayden Hays <hjh2112@hsutx.edu>
2025-07-14 23:37:36 -0500

    initial commit

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = 4000;
 
-// Serve static files from your site folder
-app.use(express.static(path.join(__dirname, 'public'))); // 👈 'public' = your site folder
 
 // Fallback to index.html for SPA routing (optional)
 app.get('*', (req, res) => {
